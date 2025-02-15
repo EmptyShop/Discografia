7d1ea05bc97c0dfff6fe5c0e1f9fd4b607ab37f5
EmptyShop <cagm202@gmail.com>
2025-02-15 11:36:04 -0600

    Actualización de versiones de librerías y scripts.
    Discografia (MVC): .NET Framework: 4.5 -> 4.8
    Discografia (WinForms): .NET Framework: 4.7.2 -> 4.8
    
    EntityFramework: 6.4.4 -> 6.5.1
    jQuery: 3.6.4 -> 3.7.1
    jQuery.Validation: 1.19.5 -> 1.21.0
    Microsoft.AspNet.Mvc: 5.2.9 -> 5.3.0
    Microsoft.AspNet.Razor: 3.2.9 -> 3.3.0
    Microsoft.AspNet.WebApi.Client: 5.2.9 -> 6.0.0 (esta versión agregó las nuevas dependencias)
    Microsoft.AspNet.WebPages: 3.2.9 -> 3.3.0
    Microsoft.Web.WebView2: 1.0.1722.45 -> 1.0.3065.39
    
    Nuevas dependencias:
    Newtonsoft.Json.Bson: 1.0.3
    System.Buffers: 4.6.0
    System.Memory: 4.6.0
    System.Numeric.Vectors: 4.6.0
    System.Runtime.CompilerServices.Unsafe: 6.1.0
    System.Threading.Tasks.Extensios: 4.6.0

--- MVCDiscografia/Scripts/_references.js
+++ MVCDiscografia/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.6.4.js" />
+﻿/// <reference path="jquery-3.7.1.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="jquery.validate.js" />
